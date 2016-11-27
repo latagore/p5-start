3d40a1b142a0b18cc4867948e434005e03bcd5fb
Steven Xu <stevenxu@live.ca>
2016-11-27 12:50:38 -0500

    add initial lightning bolt

--- sketch.js
+++ sketch.js
@@ -1,7 +1,48 @@
-function setup() {
+var perlin = new toxi.math.noise.PerlinNoise();
 
+var width = 300;
+var height = 1000;
+var x = width / 2;
+var y = 0;
+var lightningBoltPoints = [{x: x, y: y}];
+var strength = 3;
+var sum = toxi.geom.Vec2D.ZERO;
+var avg;
+var count = 1;
+
+function setup() {
+  width = 640;
+  height = 480;
+
+  createCanvas(width, height);
+  frameRate(60);
 }
 
 function draw() {
+
+  var currPoint = lightningBoltPoints[lightningBoltPoints.length - 1];
+
+  var noise = perlin.noise( currPoint.x, currPoint.y ) - 0.5;
+  var angle = strength * noise;
   
+  angle += Math.PI / 2; // initial angle downwards
+
+  var dir = toxi.geom.Vec2D.fromTheta( angle );
+  sum.addSelf(dir);
+  avg = sum.scale(1/count);
+  count++;
+  console.log(avg, count);
+  var newPoint = {
+    x: currPoint.x,
+    y: currPoint.y
+  };
+  newPoint.x += dir.x;
+  newPoint.y += dir.y;
+  lightningBoltPoints.push(newPoint);
+  
+  stroke(0);
+  lightningBoltPoints.slice(1).forEach(function(curr, i) {
+    var prev = lightningBoltPoints[i];
+    line(curr.x, curr.y, prev.x, prev.y);
+  });
 }
