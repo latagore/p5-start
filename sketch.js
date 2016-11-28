c85460eac9ff7c0132d8ab211e8b19e4356201e4
Steven Xu <stevenxu@live.ca>
2016-11-27 21:26:07 -0500

    add noise to length of vector

--- sketch.js
+++ sketch.js
@@ -5,10 +5,9 @@ var height = 1000;
 var x = width / 2;
 var y = 0;
 var lightningBoltPoints = [{x: x, y: y}];
-var strength = 3;
-var sum = toxi.geom.Vec2D.ZERO;
-var avg;
-var count = 1;
+var strength = 5;
+var fragmentMinScale = 1;
+var fragmentMaxScale = 1.1;
 
 function setup() {
   width = 640;
@@ -22,16 +21,14 @@ function draw() {
 
   var currPoint = lightningBoltPoints[lightningBoltPoints.length - 1];
 
-  var noise = perlin.noise( currPoint.x, currPoint.y ) - 0.5;
-  var angle = strength * noise;
-  
+  var noise = perlin.noise( currPoint.x, currPoint.y );
+  var angleNoise = noise - 0.5;
+  var angle = angleNoise * strength;
   angle += Math.PI / 2; // initial angle downwards
-
   var dir = toxi.geom.Vec2D.fromTheta( angle );
-  sum.addSelf(dir);
-  avg = sum.scale(1/count);
-  count++;
-  console.log(avg, count);
+  var scale = noise * (fragmentMaxScale - fragmentMinScale) + fragmentMinScale;
+  dir.scaleSelf(scale);
+
   var newPoint = {
     x: currPoint.x,
     y: currPoint.y
@@ -41,8 +38,7 @@ function draw() {
   lightningBoltPoints.push(newPoint);
   
   stroke(0);
-  lightningBoltPoints.slice(1).forEach(function(curr, i) {
-    var prev = lightningBoltPoints[i];
-    line(curr.x, curr.y, prev.x, prev.y);
-  });
+  var curr = lightningBoltPoints[lightningBoltPoints.length-1];
+  var prev = lightningBoltPoints[lightningBoltPoints.length-2];
+  line(curr.x, curr.y, prev.x, prev.y);
 }
