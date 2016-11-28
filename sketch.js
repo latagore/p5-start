7b7076292a47f6b55655af7b1f4aa5c2bf44778a
Steven Xu <stevenxu@live.ca>
2016-11-27 21:46:59 -0500

    initial lightning fork attempt

--- sketch.js
+++ sketch.js
@@ -4,41 +4,58 @@ var width = 300;
 var height = 1000;
 var x = width / 2;
 var y = 0;
-var lightningBoltPoints = [{x: x, y: y}];
 var strength = 5;
 var fragmentMinScale = 1;
-var fragmentMaxScale = 1.1;
+var fragmentMaxScale = 1.5;
+var forkChance = 0.1;
+var forkThreshold = 0.1;
+var forkAngle = Math.PI / 4;
+var bolts = [];
 
 function setup() {
   width = 640;
   height = 480;
+  // start first bolt
+  var bolt = [{x: x, y: y}];
+  bolt.angle = Math.PI / 2; // initial angle down
+  bolts.push(bolt);
 
   createCanvas(width, height);
   frameRate(60);
 }
 
 function draw() {
-
-  var currPoint = lightningBoltPoints[lightningBoltPoints.length - 1];
-
-  var noise = perlin.noise( currPoint.x, currPoint.y );
-  var angleNoise = noise - 0.5;
-  var angle = angleNoise * strength;
-  angle += Math.PI / 2; // initial angle downwards
-  var dir = toxi.geom.Vec2D.fromTheta( angle );
-  var scale = noise * (fragmentMaxScale - fragmentMinScale) + fragmentMinScale;
-  dir.scaleSelf(scale);
-
-  var newPoint = {
-    x: currPoint.x,
-    y: currPoint.y
-  };
-  newPoint.x += dir.x;
-  newPoint.y += dir.y;
-  lightningBoltPoints.push(newPoint);
-  
-  stroke(0);
-  var curr = lightningBoltPoints[lightningBoltPoints.length-1];
-  var prev = lightningBoltPoints[lightningBoltPoints.length-2];
-  line(curr.x, curr.y, prev.x, prev.y);
+  console.log(bolts.length)
+  bolts.forEach(function (bolt) {
+    var currPoint = bolt[bolt.length - 1];
+    var noise = perlin.noise( currPoint.x, currPoint.y );
+    var angleNoise = noise - 0.5;
+    var angle = angleNoise * strength;
+    angle += bolt.angle;
+    var dir = toxi.geom.Vec2D.fromTheta( angle );
+    var scale = noise * (fragmentMaxScale - fragmentMinScale) + fragmentMinScale;
+    dir.scaleSelf(scale);
+    // decide whether to add a new bolt
+    console.log(angle > strength / 2 * (1 - forkThreshold));
+    console.log(noise > (1 - forkChance));
+    if (Math.abs(angle) > strength / 2 * (1 - forkThreshold) && Math.random() > (1 - forkChance)) {
+      
+      var newBolt = [currPoint];
+      newBolt.angle = (Math.random() - 0.5) * forkAngle + angle;
+      bolts.push(newBolt);
+    }
+    
+    var newPoint = {
+      x: currPoint.x,
+      y: currPoint.y
+    };
+    newPoint.x += dir.x;
+    newPoint.y += dir.y;
+    bolt.push(newPoint);
+    
+    stroke(0);
+    var curr = bolt[bolt.length-1];
+    var prev = bolt[bolt.length-2];
+    line(curr.x, curr.y, prev.x, prev.y);
+  });
 }
